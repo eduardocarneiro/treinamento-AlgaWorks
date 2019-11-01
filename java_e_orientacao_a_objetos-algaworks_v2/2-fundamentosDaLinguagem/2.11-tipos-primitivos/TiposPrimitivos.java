5c67e963ebe889411efec07738ebd5eb5bb9ba36
Eduardo Carneiro <eduardo.oc@gmail.com>
2019-11-01 00:06:38 -0200

    added topic 2.11

---
+++ java_e_orientacao_a_objetos-algaworks_v2/2-fundamentosDaLinguagem/2.11-tipos-primitivos/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+public class TiposPrimitivos {
+
+	public static void main (String[] args) {
+	
+		boolean verdadeOuFalso = true;
+		boolean falsoOuVerdade = false;
+		char caracter = 'A';
+		char caracter2 = '7';
+		char simbolo = '@';
+		byte variavelTipoInteira = 3;
+		short variavelTipoInteira2 = 4;
+		int numero = 10;
+		long populacao = 7000000000L;
+		float numeroGrande = 9000000000F;
+		double valorReal = 5.45;
+
+
+
+	}
+}

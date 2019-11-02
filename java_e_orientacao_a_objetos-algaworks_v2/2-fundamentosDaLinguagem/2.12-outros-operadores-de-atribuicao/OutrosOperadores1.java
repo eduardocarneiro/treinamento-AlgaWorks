e007990b0ea4f65ff6ac61d3309e763caf56bcdf
Eduardo Carneiro <eduardo.oc@gmail.com>
2019-11-02 08:35:14 -0200

    added topic 2.12

---
+++ java_e_orientacao_a_objetos-algaworks_v2/2-fundamentosDaLinguagem/2.12-outros-operadores-de-atribuicao/OutrosOperadores1.java
@@ -0,0 +1,27 @@
+public class OutrosOperadores1 {
+
+	public static void main (String[] args) {
+		
+		int valor = 10;
+		
+		System.out.println("Vc pode atribuir valor: \" int valor = valor + 1;\" ");
+		System.out.println("Vc pode atribuir valor: \" int valor += 1;\" ");
+		System.out.println("Abaixo seguem algumas atribuicoes");
+
+		valor += 3; // valor igual a 13
+		System.out.println(valor);
+
+		valor -= 4; // valor igual a 9
+		System.out.println(valor);
+
+		valor *= 2; // valor igual a 18
+		System.out.println(valor);
+
+		valor /= 2; // valor igual a 9
+		System.out.println(valor);
+
+		valor %= 2; // valor igual a 1
+		System.out.println(valor);
+
+	}
+}

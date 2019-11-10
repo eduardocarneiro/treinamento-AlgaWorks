cc3ee3a9875061cb208fbd0964f66d42c05354b9
Eduardo Carneiro <eduardo.oc@gmail.com>
2019-11-10 14:54:29 -0200

    added topic 2.20

---
+++ java_e_orientacao_a_objetos-algaworks_v2/2-fundamentosDaLinguagem/2.20-exercicio_strings-entrada-de-dados_operadores-de-comparacao_e_if-else/exercise/DescobreAnoBissexto.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DescobreAnoBissexto {
+
+	public static void main (String[] args) {
+	
+	Scanner entrada = new Scanner(System.in);
+
+	// Solicitar ao usuário o ano para a verificacao
+	System.out.print("Ano: ");
+	int ano = entrada.nextInt();
+
+	if ( ano % 400 == 0) {
+		System.out.println("O ano " + ano + " é um ano bissexto");
+	
+	} else if (ano % 4 == 0) {
+		if ( ano % 100 != 0) {
+			System.out.println("O ano " + ano + " é un ano bissexto");
+		}
+	} else {
+		System.out.println("O ano " + ano + " não é um ano bissexto");
+	}
+
+	}
+}

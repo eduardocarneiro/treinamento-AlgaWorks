4e26315479eb12bb2bb74919196d046348505cfd
Eduardo Carneiro <eduardo.oc@gmail.com>
2019-11-16 02:16:40 -0200

    added topic 3.7

---
+++ java_e_orientacao_a_objetos-algaworks_v2/3-orientaoAObjetos-parte1/3.7-composicao_de_objetos/Principal2.java
@@ -0,0 +1,21 @@
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+		Veiculo carro = new Veiculo();
+		
+		carro.fabricante = "Peugeot";
+		carro.modelo = "208";
+		carro.anoDeFabricacao = 2016;
+		carro.cor = "preto";
+		
+		
+		carro.dono.nome = "Eduardo O. Carneiro";
+		carro.dono.cpf = "3347899874";
+		carro.dono.idadeProprietario = 34;
+		carro.dono.endereco = "Av. Paulista";
+		carro.dono.bairro = "Jardins";
+		carro.dono.cidade = "Sao Paulo";
+		
+	}
+}

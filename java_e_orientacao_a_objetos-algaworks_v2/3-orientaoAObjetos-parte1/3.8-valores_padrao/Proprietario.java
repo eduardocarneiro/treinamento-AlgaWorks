8e9e0ad0f13f434bedabb9956ad08225a7388977
Eduardo Carneiro <eduardo.oc@gmail.com>
2019-11-16 13:34:39 -0200

    add topic 3.8

---
+++ java_e_orientacao_a_objetos-algaworks_v2/3-orientaoAObjetos-parte1/3.8-valores_padrao/Proprietario.java
@@ -0,0 +1,11 @@
+
+public class Proprietario {
+
+	String nome;
+	String cpf;
+	int idadeProprietario;
+	String endereco;
+	String bairro;
+	String cidade;
+	
+}
---
+++ java_e_orientacao_a_objetos-algaworks_v2/3-orientaoAObjetos-parte1/3.8-valores_padrao/Veiculo.java
@@ -0,0 +1,11 @@
+
+public class Veiculo {
+
+	String fabricante;
+	String modelo;
+	String cor;
+	int anoDeFabricacao;
+	Proprietario dono;
+	
+	
+}

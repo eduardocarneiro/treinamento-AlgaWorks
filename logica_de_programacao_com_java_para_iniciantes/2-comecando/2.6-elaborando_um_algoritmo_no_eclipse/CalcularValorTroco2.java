06360ab00f683831ebb68e8b9430d1bcae3cbbed
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-02 15:59:42 -0300

    added topic 2.6

---
+++ logica_de_programacao_com_java_para_iniciantes/2-comecando/2.6-elaborando_um_algoritmo_no_eclipse/CalcularValorTroco2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CalcularValorTroco2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite o valor do produto: ");
+		Double valorProduto = scanner.nextDouble();
+		
+		System.out.print("Digite o valor passado pelo cliente: ");
+		Double valorCliente = scanner.nextDouble();
+		
+		
+		Double resultado = valorCliente - valorProduto;
+		
+		System.out.println("Valor do troco: " + resultado);
+	}
+}

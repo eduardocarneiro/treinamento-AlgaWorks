666f27d5d6d18f18c5e0b4fdd058717a970d3324
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-04 00:21:47 -0300

    added topic 3.3

---
+++ logica_de_programacao_com_java_para_iniciantes/3-variaveis_e_constantes/3.3-tipos_numeros_byte_short_integer_long/TiposNumericos.java
@@ -0,0 +1,19 @@
+
+public class TiposNumericos {
+
+	public static void main(String[] args) {
+		
+		Byte numeroByte = 1; // -128 até 127
+		
+		short numeroShort = 1; // -32768 até 32767 
+		
+		Integer numeroInteger = 1; // 2^31 = 2147483648 até (2147483648 -1) = 2147483647
+		
+		Long numeroLong = 1l; // 2^63 = -9,223372037x10^18 até 9,223372037x10^18 
+		
+		Float numeroFloat = 1.0f;
+		
+		Double numeroDouble = 1.0;
+				
+	}
+}

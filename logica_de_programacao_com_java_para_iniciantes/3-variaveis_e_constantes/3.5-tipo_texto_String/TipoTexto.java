9ea3d70b7f833f1c8833e3778e6c1d9b4bec381a
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-04 00:43:20 -0300

    added topic 3.5

---
+++ logica_de_programacao_com_java_para_iniciantes/3-variaveis_e_constantes/3.5-tipo_texto_String/TipoTexto.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TipoTexto {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite seu nome: ");
+		String nome = scanner.nextLine();
+		
+		System.out.println(nome);
+		
+		scanner.close();
+		
+		
+		
+	}
+}

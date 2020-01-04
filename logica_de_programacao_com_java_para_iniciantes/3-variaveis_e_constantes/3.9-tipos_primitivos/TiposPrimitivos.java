25c48e903febd3812ee296f2262035787bef77ba
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-04 08:17:13 -0300

    added topic 3.9

---
+++ logica_de_programacao_com_java_para_iniciantes/3-variaveis_e_constantes/3.9-tipos_primitivos/TiposPrimitivos.java
@@ -0,0 +1,28 @@
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+		
+		Byte variavelByte = 1; // tipo primitivo é byte
+		 
+		Short variavelShort = 1; // tipo primitivo é short
+		
+		Integer variavelInteger = 1; // tipo primitivo é int
+		
+		Long variavelLong = 1L; // tipo primitivo é long
+		
+		Float variavelFloat = 1.0f; // tipo primitivo é float
+		
+		Double variavelDouble = 1.0; // tipo primitivo é double
+
+		Character variavelCharacter = 'A'; // tipo primitivo é char
+		
+		Boolean variavelBoolean = true; // tipo primitivo é boolean
+		
+		// String não tem tipo primitivo
+		
+		String variavelString = "";
+		
+		
+	}
+}

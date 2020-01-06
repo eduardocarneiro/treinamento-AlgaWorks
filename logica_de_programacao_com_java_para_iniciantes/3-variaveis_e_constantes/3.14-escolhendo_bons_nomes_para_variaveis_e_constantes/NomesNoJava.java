b3359cea8a00bab9e17f73328873d9c11bb1305f
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-06 10:13:14 -0300

    added topic 3.14

---
+++ logica_de_programacao_com_java_para_iniciantes/3-variaveis_e_constantes/3.14-escolhendo_bons_nomes_para_variaveis_e_constantes/NomesNoJava.java
@@ -0,0 +1,17 @@
+
+public class NomesNoJava {
+
+	public static void main(String[] args) {
+		System.out.println("O Java utiliza o padrão \"Camel Case\"");
+		System.out.println("");
+		System.out.println("Nome de variáveis, classes e constantes");
+		System.out.println("---------------------------");
+		System.out.println("- Pode começar com qualquer letra, com $ e _");
+		System.out.println("- Pode conter números no meio e final do nome");
+		System.out.println("- Não pode iniciar com números");
+		System.out.println("- Não pode conter caracteres especiais como @, !, %, &");
+		System.out.println("- É BOM NÃO USAR ACENTOS");
+		System.out.println("- static final Integer NOME_BOM_PARA_CONSTANTE_E_DESCREVER_POR_COMPLETO_EM_MAISCULO_SEPARANDO_POR_UNDERLINE = 1;");
+		
+	}
+}

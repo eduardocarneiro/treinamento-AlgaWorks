6d00ce599fa152ca822dbec92ac0adc9c4083b81
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-06 11:17:28 -0300

    added topic 4.1

---
+++ logica_de_programacao_com_java_para_iniciantes/4-operadores/4.1-introducao_a_operadores/IntroducaoOperadores.java
@@ -0,0 +1,21 @@
+
+public class IntroducaoOperadores {
+
+	public static void main(String[] args) {
+		
+		System.out.println("[Operador]    [Tipo]           [Descrição]");
+		System.out.println("+ e -         ARITMÉTICO       Adição e subtração");
+		System.out.println("* e /         ARITMÉTICO       Multiplicação e divisão");
+		System.out.println("%             ARITMÉTICO       Módulo");
+		System.out.println("> e >=        RELACIONAL       Maior que e maior ou igual a");
+		System.out.println("< e <=        RELACIONAL       Menor que e menor ou igual a");
+		System.out.println("== e !=       RELACIONAL       Igual e diferente");
+		System.out.println("=             ATRIBUIÇÃO       Atribui algum valor para a variável");
+		System.out.println("+= e -=       ATRIBUIÇÃO       Faz a operação - adição ou subtração - e, depois, a atribuição");
+		System.out.println("*= e /=       ATRIBUIÇÃO       Faz a operação - multiplicação ou divisão - e, depois, a atribuição");
+		System.out.println("%=            ATRIBUIÇÃO       Faz a operação - módulo - e, depois, a atribuição");
+		System.out.println("&&            LÓGICO           Os dois lados do operador devem ser verdadeiros");
+		System.out.println("||            LÓGICO           Um dos lados deve ser verdadeiro");
+		System.out.println("!             LÓGICO           Reverte o valor");
+	}
+}

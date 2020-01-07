d2f30c109d72ebaf16c6e46c54aeac2f1d9919b2
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-07 10:03:46 -0300

    added topic 4.10

---
+++ logica_de_programacao_com_java_para_iniciantes/4-operadores/4.10-concatenacao_de_texto/ConcatenacaoTextos.java
@@ -0,0 +1,12 @@
+
+public class ConcatenacaoTextos {
+
+	public static void main(String[] args) {
+		
+		String inicio = "Este é o início, ";
+		String meio = "aqui é o meio, ";
+		String fim = "por fim, este é o fim do texto.";
+				
+		System.out.println(inicio + meio + fim);
+	}
+}

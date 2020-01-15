56042173f59a14b865a4d46881df99c2ff4841c3
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-15 14:12:38 -0300

    added topic 11.3 v2

--- logica_de_programacao_com_java_para_iniciantes/11-utilizando_codigo_de_outros_programadores/11.3-utilizando_uma_biblioteca_de_terceiro_para_envio_de_emails/UsandoBibliotecaDeEmail.java
+++ logica_de_programacao_com_java_para_iniciantes/11-utilizando_codigo_de_outros_programadores/11.3-utilizando_uma_biblioteca_de_terceiro_para_envio_de_emails/UsandoBibliotecaDeEmail.java
@@ -5,20 +5,21 @@ import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
 
-public class Exercicio01EnviaEmail {
+public class UsandoBibliotecaDeEmail {
 
-	static void EnviarEmail (String para, String assunto, String msg) throws EmailException {
-	
+	public static void main(String[] args) throws EmailException {
 		Email email = new SimpleEmail();
 		email.setHostName("smtp.gmail.com");
 		email.setSmtpPort(465);
 		email.setAuthenticator(new DefaultAuthenticator("ecarneiro.redhat", "senha"));
 		email.setSSLOnConnect(true);
 		email.setFrom("ecarneiro.redhat@gmail.com");
-		email.setSubject(assunto);
-		email.setMsg(msg);
-		email.addTo(para);
+		email.setSubject("TestMail");
+		email.setMsg("This is a test mail ... :-)");
+		email.addTo("eduardo.oc@gmail.com");
 		email.send();
-		
+		System.out.println("E-mail enviado com sucesso!");
+
 	}
+
 }

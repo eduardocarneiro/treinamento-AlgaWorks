9a64fa276cb8c47c464b1bf09d5b4c7c40455bfd
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-18 14:43:07 -0300

    added topic 14.4

---
+++ logica_de_programacao_com_java_para_iniciantes/14-criando_sua_primeira_aplicacao/14.4-listando_os_contatos_na_pagina/AlgaWorksContatos/src/main/java/com/algaworks/ContatosControle.java
@@ -0,0 +1,32 @@
+package com.algaworks;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ContatosControle {
+
+	private static final ArrayList<Contato> LISTA_CONTATOS = new ArrayList<>();
+	
+	static {
+		LISTA_CONTATOS.add(new Contato("1", "Eduardo", "+55 11 94286 9687"));
+		LISTA_CONTATOS.add(new Contato("1", "Dezenatty", "+55 11 94286 9687"));
+		LISTA_CONTATOS.add(new Contato("1", "Sarah", "+55 11 94286 9687"));
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/contatos")
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("listar");
+		modelAndView.addObject("contatos", LISTA_CONTATOS);
+		return modelAndView;	
+	}
+	
+}

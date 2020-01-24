32d1a268fde46c9ee23cc71ff4272f2e2a56ac8d
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-01-24 11:13:25 -0300

    added topic 3.10

---
+++ java_e_orientacao_a_objetos-algaworks_v2/3-orientaoAObjetos-parte1/3.10-criando_nomeando_e_chamando_metodos/Principal3.java
@@ -0,0 +1,21 @@
+
+public class Principal3 {
+
+	public static void main(String[] args) {
+	
+		Veiculo3 veiculo1 = new Veiculo3();
+		veiculo1.modelo = "208";
+		
+		
+		Veiculo3 veiculo2 = new Veiculo3();
+		veiculo2.modelo = "onix";
+		
+		
+		veiculo1.ligarCarro();
+		veiculo2.ligarCarro();
+		
+		
+
+	}
+
+}
---
+++ java_e_orientacao_a_objetos-algaworks_v2/3-orientaoAObjetos-parte1/3.10-criando_nomeando_e_chamando_metodos/Veiculo3.java
@@ -0,0 +1,13 @@
+
+public class Veiculo3 {
+
+	String fabricante;
+	String modelo;
+	int anoDeFabricacao;
+	
+	void ligarCarro() {
+		
+		System.out.println("Ligando o veiculo: " + modelo);
+	}
+	
+}
